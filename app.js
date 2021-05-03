3734b3d3e2b8d7f6fdf04789c8ddbe6426a8ffb6
arturpilarek <artur.pilarek@gmail.com>
2021-05-03 14:15:35 +0200

    2 questions added

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const quizButton = document.querySelector(".knap")
 const overlay = document.querySelector("#overlay")
 const quizBox = document.querySelector("#quiz__box")
+const quizContent = document.querySelector(".question__content")
 const exitIcon = document.querySelector("#exitIcon")
+const questionNumber = document.querySelector(".quiz-box__question-number")
 // Password variables
 const passwordInput = document.querySelector("#passwordInput")
 const passwordSubmit = document.querySelector(".tjek__submit")
 const errorListe = document.querySelector(".errorListe")
 const errorContainer = document.querySelector(".error")
+var body = document.getElementsByTagName("body")[0]
 
 let ErrorArray = []
 console.log(errorContainer.firstChild)
@@ -19,7 +22,11 @@ function triggerModal() {
   overlay.classList.add("overlay__active")
   quizBox.style.display = "block"
   document.body.style.overflow = "hidden"
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
 }
+
+//
 
 exitIcon.addEventListener("click", () => {
   closeModal()
@@ -32,18 +39,88 @@ function closeModal() {
 }
 
 // Spørgesmål 1
+const form1 = document.querySelector(".quiz__form")
 
 const checkButton1 = document.querySelector(".checkAnswer-1")
 const answers1 = Array.from(document.getElementsByName("question1"))
+const result1 = document.querySelector(".question1__result")
+const answerPresentation1 = document.querySelector(".question__content-svar")
+const answer1 = document.querySelector(".answer1")
+const nextQuestionButton1 = document.querySelector(".next-question-1")
 
 checkButton1.addEventListener("click", () => {
   let answers1Filtered = answers1.filter((selectedItem) => {
     return selectedItem.checked
   })
-  console.log(answers1Filtered)
+  if (answers1Filtered[0].value === "Btrue") {
+    quizBox.style.backgroundColor = "green"
+    quizContent.children[0].textContent = "Tillykke, dit svar er rigtigt!"
+    quizContent.children[0].style.color = "green"
+    form1.classList.add("hide")
+    checkButton1.classList.add("hide")
+    result1.classList.remove("hide")
+    answerPresentation1.classList.remove("hide")
+    answer1.classList.remove("hide")
+    answer1.textContent = "Falsk"
+  } else {
+    quizBox.style.backgroundColor = "red"
+    quizContent.children[0].textContent = "Desværre, dit svar er forkert"
+    quizContent.children[0].style.color = "red"
+    form1.classList.add("hide")
+    checkButton1.classList.add("hide")
+    result1.classList.remove("hide")
+    answerPresentation1.classList.remove("hide")
+    answer1.classList.remove("hide")
+    answer1.textContent = "Falsk"
+  }
 })
 
-function checkAnswer() {}
+// Spørgesmål 2
+
+const form2 = document.querySelector(".quiz__form2")
+const checkButton2 = document.querySelector(".checkAnswer-2")
+const answers2 = Array.from(document.getElementsByName("question2"))
+const result2 = document.querySelector(".question2__result")
+
+nextQuestionButton1.addEventListener("click", () => {
+  result1.classList.add("hide")
+  answer1.classList.add("hide")
+  answerPresentation1.classList.add("hide")
+  quizContent.children[0].style.color = "#006df0"
+  quizContent.children[0].innerHTML =
+    "Hvilke tegn skal du bruge i en adgangskode for at gøre den stærk?"
+  questionNumber.innerHTML = "Spørgsmål 2 ud af 8"
+  quizBox.style.backgroundColor = "#006df0"
+  form2.classList.remove("hide")
+  checkButton2.classList.remove("hide")
+})
+
+checkButton2.addEventListener("click", () => {
+  let answers1Filtered = answers2.filter((selectedItem) => {
+    return selectedItem.checked
+  })
+  if (answers1Filtered[0].value === "Dtrue") {
+    quizBox.style.backgroundColor = "green"
+    quizContent.children[0].textContent = "Tillykke, dit svar er rigtigt!"
+    quizContent.children[0].style.color = "green"
+    form2.classList.add("hide")
+    checkButton2.classList.add("hide")
+    result2.classList.remove("hide")
+    answerPresentation1.classList.remove("hide")
+    answer1.classList.remove("hide")
+    answer1.textContent = "Alle de ovenstående"
+  } else {
+    quizBox.style.backgroundColor = "red"
+    quizContent.children[0].textContent = "Desværre, dit svar er forkert"
+    quizContent.children[0].style.color = "red"
+    form2.classList.add("hide")
+    checkButton2.classList.add("hide")
+    result2.classList.remove("hide")
+    answerPresentation1.classList.remove("hide")
+    answer1.classList.remove("hide")
+    answer1.textContent = "Alle de ovenstående"
+  }
+})
 
 // Password tjekker
 
